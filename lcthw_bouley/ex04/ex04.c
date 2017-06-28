5774ea5b27365a1ac4a0177bf1a40928dd176fa6
Charles Bouley <charlesdavidbouley@gmail.com>
2017-06-28 09:45:19 -0400

    20170628 LCTHW Commit
    20170628 LCTHW Commit

---
+++ lcthw_bouley/ex04/ex04.c
@@ -0,0 +1,15 @@
+// include the standard input/output header file which includes printf
+#include <stdio.h>
+
+// Warning!! This program is wrong on purpose - testing the debugger
+
+int main()
+{
+	int age = 10;
+	int height;
+	
+	printf("I am %d years old.\n");
+	printf("I am %d inches tall.\n", height);
+	
+	return 0;
+}
